3b781203ace0a362ee81826d27ab9605ac0086d0
murtuza2905ali <murtuza505ali@gmail.com>
2025-08-05 19:00:57 +0530

    Update settings.py

--- sampada_scraper/settings.py
+++ sampada_scraper/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-very-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
